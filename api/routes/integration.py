88803b727b22b9685c56d74d62c655e1edf4d830
Blessing Etuk <112414755+Tukkieee@users.noreply.github.com>
2025-02-23 00:51:56 +0100

    Update integration.py

--- api/routes/integration.py
+++ api/routes/integration.py
@@ -7,7 +7,7 @@ router = APIRouter()
 @router.get("/integration.json")
 async def integration(request: Request):
     # base_url = str(request.base_url).rstrip("/")
-    base_url = "https://tqvjrlgn-8000.uks1.devtunnels.ms"
+    base_url = "https://network-health-path.onrender.com"
     # telex_channel_webhook = "https://ping.telex.im/v1/webhooks/019519ce-8dd0-77d8-a5b4-20227ee0b485"
     return{
         "data" : {
