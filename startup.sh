22b161df868c0a95a37b8248f694b923afbd5950
Tukkieee <etuk2003@gmail.com>
2025-02-23 16:36:36 +0100

    Install mtr in Dockerfile

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Update package lists and install mtr
-sudo apt-get update && sudo apt-get install -y mtr
-
-echo "Starting FastAPI app..."
-uvicorn main:app --host 0.0.0.0 --port $PORT
